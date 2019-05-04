cd209a7caf96fa2197089a4832f809b07fe311b8
Jorge Cascante <cascante.george@gmail.com>
2019-05-04 09:33:26 -0600

    removes code

--- to-do-react/app/redux/reducer.js
+++ to-do-react/app/redux/reducer.js
@@ -4,9 +4,7 @@ import { createAction, handleActions } from 'redux-actions';
 
 // Acctions
 export const GET_ALL_TASKS = createAction('GET_ALL_TASKS');
-export const ADD_NEW_TASK = createAction('ADD_NEW_TASK');
 export const UPDATE_TASK = createAction('UPDATE_TASK');
-export const DELETE_TASK = createAction('DELETE_TASK');
 export const ACTIVATE_TASK_EDIT = createAction('ACTIVATE_TASK_EDIT');
 
 const TodoBackend = Axios.create({
@@ -24,15 +22,9 @@ export default handleActions({
   GET_ALL_TASKS: (state, action) => {
     return Object.assign({}, state, { tasks: action.payload });
   },
-  ADD_NEW_TASK: (state, action) => {
-    return Object.assign({}, state, { tasks: [action.payload].concat(state.tasks) });
-  },
   UPDATE_TASK: (state, action) => {
     const tasks = _.map(state.tasks, task => (task.id === action.payload.id ? action.payload : task));
     return Object.assign({}, state, { tasks, taskToEdit: 0 });
-  },
-  DELETE_TASK: (state, action) => {
-    return Object.assign({}, state, { tasks: _.filter(state.tasks, task => task.id !== action.payload) });
   },
   ACTIVATE_TASK_EDIT: (state, action) => {
     return Object.assign({}, state, { taskToEdit: action.payload });
@@ -45,28 +37,6 @@ export const getAllTasks = () => {
     try {
       const { data } = await TodoBackend.get('tasks');
       dispatch(GET_ALL_TASKS(data.tasks));
-    } catch (err) {
-      console.log(err);
-    }
-  };
-};
-
-export const addNewTask = (description) => {
-  return async (dispatch) => {
-    try {
-      const { data } = await TodoBackend.post(`task/create/${description}`);
-      dispatch(ADD_NEW_TASK(data.task));
-    } catch (err) {
-      console.log('errrrr', err);
-    }
-  };
-};
-
-export const deleteTask = (id) => {
-  return async (dispatch) => {
-    try {
-      await TodoBackend.delete(`task/delete/${id}`);
-      dispatch(DELETE_TASK(id));
     } catch (err) {
       console.log(err);
     }
